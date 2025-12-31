2b65105d8b5575681e4be2b7913928571f7d5576
picabob3-hub <picabob3@gmail.com>
2025-12-31 09:14:06 -0500

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,13 +1,10 @@
-const CACHE_NAME = 'viva-v1';
+const CACHE_NAME = 'viva-v2'; // CAMBIADO A V2 PARA FORZAR ACTUALIZACIÓN
+const assets = ['./', './index.html', './VIVAICONO.PNG'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html']))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
